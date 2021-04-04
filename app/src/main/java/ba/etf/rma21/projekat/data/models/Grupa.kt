5e52c759821e059b553e19d0fe2847ca2104bb59
plvr99 <apalavra2@etf.unsa.ba>
2021-04-04 17:28:59 +0200

    Dodane overridane toString metode

--- app/src/main/java/ba/etf/rma21/projekat/data/models/Grupa.kt
+++ app/src/main/java/ba/etf/rma21/projekat/data/models/Grupa.kt
@@ -1,4 +1,7 @@
 package ba.etf.rma21.projekat.data.models
 
 data class Grupa(val naziv: String, val nazivPredmeta: String) {
+    override fun toString(): String {
+        return naziv
+    }
 }
--- app/src/main/java/ba/etf/rma21/projekat/data/models/Predmet.kt
+++ app/src/main/java/ba/etf/rma21/projekat/data/models/Predmet.kt
@@ -1,4 +1,7 @@
 package ba.etf.rma21.projekat.data.models
 
 data class Predmet(val naziv: String, val godina: Int) {
+    override fun toString(): String {
+        return naziv
+    }
 }
